3177b65cb6ac9a2410ffafeaeb38dd9587042ffb
Chris Casey <cdcasey@gmail.com>
2020-08-02 19:23:04 -0500

    prettier works w/ airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,8 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  //   extends: ['eslint:recommended'],
-  extends: ['airbnb-base'],
+  //   extends: ['eslint:recommended', 'eslint-config-prettier'],
+  extends: ['airbnb-base', 'eslint-config-prettier'],
   parserOptions: {
     ecmaVersion: 11,
     sourceType: 'module',
