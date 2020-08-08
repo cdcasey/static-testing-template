960e970c5a0f5e29be7c04ffd3956eb75e5628a2
Chris Casey <cdcasey@gmail.com>
2020-08-07 23:35:04 -0500

    Make eslint parse TS files & play w/ prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,4 +13,19 @@ module.exports = {
     },
   },
   rules: {},
+  overrides: [
+    {
+      files: '**/*.+(ts|tsx)',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'eslint-config-prettier/@typescript-eslint',
+      ],
+    },
+  ],
 };
